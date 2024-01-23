05b2dae2fc877951af6e381427e06072418ce2a1
nadavw <nadav.weissman.01@gmail.com>
2024-01-23 14:37:36 +0200

    remove endpoint creation and deletion

--- VxAIMngDelpoy.sh
+++ VxAIMngDelpoy.sh
@@ -30,9 +30,8 @@ if [ "$ACTION" != "DEPLOY" ] && [ "$ACTION" != "UNDEPLOY" ] ; then
 fi
 
 # Define variables
-REGION=us-central1
-ENDPOINT_ID=100
-ENDPOINT_NAME="stable-diffusion-endpoint"
+PROJECT=$(gcloud config get-value project)
+REGION=$(gcloud config get-value compute/region)
 DEPLOY_MODEL_ID=1234
 MODEL_NAME="stable_diffusion_1_5-unique"
 MACHINE_TYPE="n1-standard-8"
@@ -47,15 +46,16 @@ if [ -z "$MODEL_ID" ]; then
   exit 1
 fi
 
-if [ "$ACTION" == "DEPLOY" ]; then
-  # Endpoint creation
-  echo "Creating endpoint..."
-  gcloud ai endpoints create --region=$REGION --endpoint-id=$ENDPOINT_ID --display-name=$ENDPOINT_NAME
-  if [ $? -ne 0 ]; then
-    echo "Error: Endpoint creation failed. Exiting script."
-    exit 1
-  fi
+ENDPOINT_ID=$()gcloud ai endpoints list --format='value(ENDPOINT_ID)')
 
+# Check if the model ID is empty
+if [ -z "ENDPOINT_ID" ]; then
+  echo "Error: ENDPOINT ID not found. Exiting script."
+  exit 1
+fi
+
+
+if [ "$ACTION" == "DEPLOY" ]; then
   # Model deploy (takes time)
   echo "Deploying model..."
   gcloud ai endpoints deploy-model $ENDPOINT_ID --region=$REGION --model=$MODEL_ID --display-name=$MODEL_NAME --machine-type=$MACHINE_TYPE --accelerator=count=1,type=$ACCELERATOR_TYPE --deployed-model-id=$DEPLOY_MODEL_ID
@@ -73,16 +73,7 @@ if [ "$ACTION" == "UNDEPLOY" ]; then
     echo "Error: Model undeployment failed. Exiting script."
     exit 1
   fi
-
-  # Endpoint deletion
-  echo "Deleting endpoint..."
-  gcloud ai endpoints delete $ENDPOINT_ID --region=$REGION --project=nadav-playground
-  if [ $? -ne 0 ]; then
-    echo "Error: Endpoint deletion failed. Exiting script."
-    exit 1
-  fi
 fi
-
 
 echo "Script completed successfully."
 exit 0
